58f603655144002c333494da2af1c8fd6e9bfff9
Mahmoud_Islam <mahmoudislam.1.cs@gmail.com>
2026-03-02 16:57:41 +0200

    Move SQL files and remove Modification.sql

--- sql/build/Modification.sql
+++
@@ -1,8 +0,0 @@
-USE master;
-GO
-ALTER DATABASE BrainRotAnalysis 
-MODIFY FILE ( NAME = BrainRotAnalysis, FILEGROWTH = 10% );
-GO
-ALTER DATABASE BrainRotAnalysis 
-MODIFY FILE ( NAME = BrainRotAnalysis_log, FILEGROWTH = 10% );
-GO
